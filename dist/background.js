5784ccc954d330d72cf2db362b9f18578747a769
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-08-21 12:30:30 -0400

    add default links to localStorage

--- dist/background.js
+++ dist/background.js
@@ -1,5 +1,33 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ infoo: 'Your data here' }, function () {
+    chrome.storage.local.set({
+            links: [
+                {
+                    url: "https://outlook.office.com/mail/",
+                    img: "imgs/outlook.png",
+                    alt: "Outlook"
+                },
+                {
+                    url: "https://bca.schoology.com/home",
+                    img: "imgs/schoology.webp",
+                    alt: "Schoology"
+                },
+                {
+                    url: "https://bcts.powerschool.com/public/",
+                    img: "imgs/powerschool.png",
+                    alt: "Powerschool"
+                },
+                {
+                    url: "https://drive.google.com",
+                    img: "imgs/drive.png",
+                    alt: "Google Drive"
+                },
+                {
+                    url: "https://www.github.com",
+                    img: "imgs/github.png",
+                    alt: "GitHub"
+                },
+            ]
+        }, function () {
         console.log('Data is stored');
     });
 });
